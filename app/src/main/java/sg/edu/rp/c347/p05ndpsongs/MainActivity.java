9d360af80dd8244a679e4c86f2841456ce87ee01
15035634 <15035634@myrp.edu.sg>
2017-05-18 13:35:31 +0800

    stuff

--- app/src/main/java/sg/edu/rp/c347/p05ndpsongs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p05ndpsongs/MainActivity.java
@@ -2,12 +2,26 @@ package sg.edu.rp.c347.p05ndpsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etSongname, etSingers, etYear;
+    RadioGroup rgStars;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etSongname = (EditText)findViewById(R.id.etSong);
+        etSingers = (EditText)findViewById(R.id.etSingers);
+        etYear = (EditText)findViewById(R.id.etYear);
+        rgStars = (RadioGroup)findViewById(R.id.rgStar);
+
+
+
     }
 }
