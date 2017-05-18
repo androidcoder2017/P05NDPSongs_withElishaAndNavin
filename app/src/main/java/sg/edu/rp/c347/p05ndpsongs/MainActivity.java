9e79fb608d511e9d5f5dc6d4e6dfd4befba5b7e1
15035634 <15035634@myrp.edu.sg>
2017-05-18 14:38:08 +0800

    stuff 2

--- app/src/main/java/sg/edu/rp/c347/p05ndpsongs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p05ndpsongs/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 rgStars.getCheckedRadioButtonId();
                 DBHelper dbh = new DBHelper(MainActivity.this);
                 //long row_affected = dbh.insertSong(data);
+                dbh.close();
             }
         });
 
