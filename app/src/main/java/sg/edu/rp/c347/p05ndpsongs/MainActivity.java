f399cd6451692215a3dc3ff1d8f373a969e9692a
15035634 <15035634@myrp.edu.sg>
2017-05-18 14:34:02 +0800

    stuff 2

--- app/src/main/java/sg/edu/rp/c347/p05ndpsongs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p05ndpsongs/MainActivity.java
@@ -2,6 +2,8 @@ package sg.edu.rp.c347.p05ndpsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
@@ -9,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText etSongname, etSingers, etYear;
     RadioGroup rgStars;
+    Button insert, show;
 
 
     @Override
@@ -20,7 +23,28 @@ public class MainActivity extends AppCompatActivity {
         etSingers = (EditText)findViewById(R.id.etSingers);
         etYear = (EditText)findViewById(R.id.etYear);
         rgStars = (RadioGroup)findViewById(R.id.rgStar);
+        insert = (Button)findViewById(R.id.buttInsert);
+        show = (Button)findViewById(R.id.buttShow);
 
+
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             String data =  etSongname.getText().toString() + etSingers.getText().toString() + etYear.getText().toString() + rgStars.getCheckedRadioButtonId();
+
+                rgStars.getCheckedRadioButtonId();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                //long row_affected = dbh.insertSong(data);
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
---
+++ app/src/main/java/sg/edu/rp/c347/p05ndpsongs/Songs.java
@@ -0,0 +1,66 @@
+package sg.edu.rp.c347.p05ndpsongs;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15035634 on 18/5/2017.
+ */
+
+public class Songs implements Serializable {
+
+    private  int id;
+    private String songTitle;
+    private String singers;
+    private String year;
+    private int star;
+
+    public Songs(int id, String songTitle, String singers, String year, int star) {
+        this.songTitle = songTitle;
+        this.singers = singers;
+        this.year = year;
+        this.id = id;
+        this.star = star;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSongtitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+
+    public void setSingers(String singers) {
+        this.singers = singers;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+}
+
